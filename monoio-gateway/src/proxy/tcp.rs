decdc853335bb7c6d08dee9db38195e499c19d0e
Kingtous <kingtous@qq.com>
2022-07-18 15:37:03 +0800

    opt: remove nested spawn

--- monoio-gateway/src/proxy/tcp.rs
+++ monoio-gateway/src/proxy/tcp.rs
@@ -30,24 +30,21 @@ impl TcpProxy {
         let listener = TcpListener::bind_with_config(local_addr, &listener_config)
             .expect(&format!("cannot bind with address: {}", local_addr));
         // start io loop
-        let io = monoio::spawn(async move {
-            loop {
-                let (mut conn, _addr) = listener.accept().await.expect("accept failed");
-                println!("peer addr is {:?}", peer_addr);
-                let mut remote_conn = TcpStream::connect_addr(peer_addr)
-                    .await
-                    .expect(&format!("unable to connect addr: {}", peer_addr));
+        loop {
+            let (mut conn, _addr) = listener.accept().await.expect("accept failed");
+            println!("peer addr is {:?}", peer_addr);
+            let mut remote_conn = TcpStream::connect_addr(peer_addr)
+                .await
+                .expect(&format!("unable to connect addr: {}", peer_addr));
 
-                let (mut local_read, mut local_write) = conn.split();
-                let (mut remote_read, mut remote_write) = remote_conn.split();
-                let _ = monoio::join!(
-                    copy_data(&mut local_read, &mut remote_write),
-                    copy_data(&mut remote_read, &mut local_write)
-                );
-                println!("transfer finished");
-            }
-        });
-        io.await;
+            let (mut local_read, mut local_write) = conn.split();
+            let (mut remote_read, mut remote_write) = remote_conn.split();
+            let _ = monoio::join!(
+                copy_data(&mut local_read, &mut remote_write),
+                copy_data(&mut remote_read, &mut local_write)
+            );
+            println!("transfer finished");
+        }
         Ok(())
     }
 }
